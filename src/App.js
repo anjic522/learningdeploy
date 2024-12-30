f17bdf055493e8b85dc507d18409a1e9e37826f6
1999BHARGAVI <anjic522@gmail.com>
2024-12-30 11:41:44 +0530

    hii

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Newcomponent from './tasks/new';
+
+function App() {
+  return (
+    <div>
+      <Newcomponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tasks/new.jsx
@@ -0,0 +1,8 @@
+const Newcomponent=()=>{
+    return(
+        <div>
+            <h1> hiii this is Anji</h1>
+        </div>
+    )
+}
+export default Newcomponent
